0abe30584376a576ccd52ac7cf9a4bd1dc8cd142
u201603806 <69479972+u201603806@users.noreply.github.com>
2020-08-24 14:02:22 -0500

    Signer rest
    Servicios de firma digital

---
+++ Signer-rest/src/main/java/com/upc/signer/SignerRestApplication.java
@@ -0,0 +1,13 @@
+package com.upc.signer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignerRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignerRestApplication.class, args);
+	}
+
+}
