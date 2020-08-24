f4265ef038847f793a695ff19dae6ea6b5f88164
u201603806 <69479972+u201603806@users.noreply.github.com>
2020-08-24 14:03:55 -0500

    Util Rest
    Servicio de utilidades

---
+++ Util-rest/src/main/java/com/upc/util/UtilRestApplication.java
@@ -0,0 +1,13 @@
+package com.upc.util;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilRestApplication.class, args);
+	}
+
+}
---
+++ Util-rest/src/test/java/com/upc/util/UtilRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.util;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
