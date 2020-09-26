606b032aeb50ff28ebb94434093d3dcebc28400b
Cesar Huachua Llanos <u201603806@upc.edu.pe>
2020-09-25 21:36:54 -0500

    acceso de colaboradores

--- ContractCli-rest/src/main/java/com/upc/contract/cli/controller/LoginController.java
+++ ContractCli-rest/src/main/java/com/upc/contract/cli/controller/LoginController.java
@@ -1,12 +1,12 @@
 package com.upc.contract.cli.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.upc.contract.core.entity.Usuario;
 
 
 public interface LoginController {
-	@GetMapping("/entidad/{codigo}")
-    Usuario obtenerEntidad(@PathVariable(value = "codigo") Long codigo);
+	@PostMapping("/acceso")
+    Usuario obtenerEntidad(@RequestBody Usuario usuario);
 }
--- ContractCli-rest/src/main/java/com/upc/contract/cli/controller/impl/LoginControllerImpl.java
+++ ContractCli-rest/src/main/java/com/upc/contract/cli/controller/impl/LoginControllerImpl.java
@@ -4,8 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -15,22 +15,22 @@ import com.upc.contract.core.entity.Usuario;
 import com.upc.contract.core.service.UsuarioService;
 
 @RestController
-@RequestMapping("/cliente")
+@RequestMapping("/seguridad")
 public class LoginControllerImpl implements LoginController {
 	@Autowired
 	private UsuarioService usuarioService;
 	Logger logger = LoggerFactory.getLogger(LoginControllerImpl.class);
 
 	@Override
-	@GetMapping("/entidad/{codigo}")
-	public Usuario obtenerEntidad(@PathVariable(value = "codigo") Long codigo) {
-		Usuario oUsuario;
+	@PostMapping("/acceso")
+	public Usuario obtenerEntidad(@RequestBody Usuario usuario) {
+		Usuario oUsuario = new Usuario();
 		try {
 			logger.debug("Buscando entidad usuario");
-			oUsuario = usuarioService.obtenerUsuario(codigo);
+			oUsuario = usuarioService.userValidate(usuario.getCodUsuario(), usuario.getClave());
 		} catch (Exception e) {
-			logger.error("Error de Obtener Entidad");
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Mi mensaje");
+			logger.error("Error de Obtener colaborador");
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
 		}
 		return oUsuario;
 	}
